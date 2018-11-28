f9f4224f776f38350c8b4bafe83acb76342d12db
Christian Rios <carios23@hotmail.com>
2018-11-28 12:54:21 -0500

    Execute create-react-app on gitbash to create frontend layer

---
+++ src/main/resources/static/precache-manifest.03dd187d217d4b7275c27b419715f3c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f610b4ba2286f86e6879",
+    "url": "/static/js/main.f610b4ba.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "f610b4ba2286f86e6879",
+    "url": "/static/css/main.2fa81dac.chunk.css"
+  },
+  {
+    "revision": "0c87f9521d7874cb225b01cd198f8086",
+    "url": "/index.html"
+  }
+];
