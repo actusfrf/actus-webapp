b70b0604d70d07df42c86398113124086eef5aef
Christian Rios <carios23@hotmail.com>
2019-03-10 21:51:19 -0500

    Add Cross Origin header

--- src/main/java/com/bundiconsulting/actuswebapp/controllers/EventController.java
+++ src/main/java/com/bundiconsulting/actuswebapp/controllers/EventController.java
@@ -9,6 +9,7 @@ import org.actus.events.ContractEvent;
 import org.actus.externals.RiskFactorModelProvider;
 import org.actus.states.StateSpace;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,7 @@ class MarketModel implements RiskFactorModelProvider {
 
     // String -> ArrayList<ContractEvent>
     @RequestMapping(method=RequestMethod.POST, value="/events")
+    @CrossOrigin(origins = "http://localhost:3000")
     public List<Event> solveContract(@RequestBody Map<String, Object> json ) {
 
         Map<String,String> map = new HashMap<String,String>();
