9bef2591cad02a01dcb4cc0029d1b756b8f4cbf1
frchong247 <francisco.chong@gmail.com>
2019-03-11 22:16:04 -0500

    Update form

--- frontend/src/components/Form/index.js
+++ frontend/src/components/Form/index.js
@@ -76,33 +76,12 @@ export class Form extends PureComponent {
             'withCredentials': true,
             'credentials': 'omit'
         }
-        axios.post('http://localhost/events',tf, config)
+        axios.post('http://190.141.20.26/events',tf)
             .then(res => {
                 console.log(res);
                 console.log(res.data);
             });
-
-        // this.postData(`http://localhost/events`,tf)
-        //     .then(data => console.log(JSON.stringify(data)))
-        //     .catch(error => console.error(error));
     }
-    // postData(url = ``, data = {}) {
-    // // Default options are marked with *
-    //     return fetch(url, {
-    //         method: "POST", // *GET, POST, PUT, DELETE, etc.
-    //         mode: "no-cors", // no-cors, cors, *same-origin
-    //         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    //         credentials: "omit", // include, *same-origin, omit
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //             // "Content-Type": "application/x-www-form-urlencoded",
-    //         },
-    //         redirect: "follow", // manual, *follow, error
-    //         referrer: "no-referrer", // no-referrer, *client
-    //         body: JSON.stringify(data), // body data type must match "Content-Type" header
-    //     })
-    //     .then(response => response.json()); // parses response to JSON
-    // }
 
     validateFields(){
         let fields = this.state.requiredFields;
