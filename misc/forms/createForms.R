2ec94d7793f7795c9055d16eac0caa360aa6dc0a
Nils Bundi <nils.bundi@gmail.com>
2019-04-12 02:59:11 +0200

    fixed various bugs in forms and generator script

---
+++ misc/forms/createForms.R
@@ -0,0 +1,55 @@
+
+# global options
+options(stringsAsFactors = FALSE)
+
+# load libraries
+library(jsonlite)
+library(magrittr)
+library(readr)
+
+# define paths
+forms_path <- './json/'
+
+# define contracts to create forms for
+build_contracts <- c("CSH","UMP","CLM","PAM","LAM","LAX","ANN","NAM","STK","COM","CEG","CEC","SWPPV","SWAPS","FXOUT","FUTUR","OPTNS","CAPFL")
+
+
+# read data dictionary
+rawdat <- read_csv("Consolidated DD CTD v1.0.csv", skip=1)
+
+# convert enum values to json array
+rawdat$'Allowed Values' <- sapply(sapply(sapply(rawdat$'Allowed Values',strsplit,"\n"),strsplit,"="),function(x) paste0("[",paste(sapply(x,function(y) trimws(y[1])),collapse=","),"]"))
+
+# define json base form
+base_form = data.frame(Identifier = "<Identifier>",
+                       ContractType = "<Contract Type>",
+                       Description = "TODO: Contract description here!",
+                       Version = format(Sys.Date(), format = "%Y%m%d"),
+                       Terms = "<Terms>")
+
+# create forms by contract
+for(ct in build_contracts) {
+	
+	# prepare json form
+	form = base_form
+	form$Identifier = paste0("form_",ct)
+	form$ContractType = ct
+	jsonForm = toJSON(unbox(form), pretty=TRUE)
+
+	# extract contract specific terms
+	terms = rawdat[	which(!is.na(rawdat[[ct]])),
+			c("Group Name", "ACTUS Name", "Data Format", "Allowed Values", "Attribute Description", ct) ]
+
+	# update column names
+    	colnames(terms) = c("Group", "Name", "Type", "List", "Description", "Applicability")
+
+	# fix ContractType enum to ct
+	terms[which(terms$Name=="ContractType"),"List"]="[PAM]"
+
+	# combine form-json and terms-json
+	jsonTerms = unbox(toJSON(terms, auto_unbox=TRUE,pretty=TRUE))
+  	jsonForm = gsub("\"<Terms>\"", jsonTerms, jsonForm)
+  
+  	# export final form
+	jsonForm %>% write_lines(paste0(forms_path,'form_',ct,'.json'))
+}
--- misc/forms/forms/import.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-for %%f in (*.json) do (
-    "mongoimport.exe" --db actusweb --collection forms --file %%~nf.json
-)
