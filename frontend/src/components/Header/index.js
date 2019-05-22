83987ad23da2e559e54a03c1f0acbbf80ea3c541
Nils Bundi <nils.bundi@gmail.com>
2019-05-22 23:12:50 +0200

    added app title to header component

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -4,11 +4,10 @@ import './Header.css';
 export default class Header extends PureComponent { 
     render(){
         return (
-		<div className="header-wrapper">
-			<a href="https://actusfrf.org" target="_blank">
-	                	<img src="https://raw.githubusercontent.com/actusfrf/actus-resources/master/logos/actus_logo.jpg" width="200" alt="ACTUS Logo"/>
-                	</a>
-		</div>
+	<header>
+        	<a href="/"><img class="header-img" src ="https://raw.githubusercontent.com/actusfrf/actus-resources/master/logos/actus_logo.jpg" alt="ACTUS logo"/></a>
+        	<h1>The ACTUS Demo App</h1>
+    	</header>
         )
     }
 }
